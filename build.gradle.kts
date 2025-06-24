7598be9dde2c827afdf170dc957fe5d6f4505daa
thedev-junyoung <thedev.junyoung@gmail.com>
2025-06-25 01:43:09 +0900

    chore: add Gradle dependency - JPA, jbcrypt

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,6 +23,8 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.mindrot:jbcrypt:0.4")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 
 
 	// Test
