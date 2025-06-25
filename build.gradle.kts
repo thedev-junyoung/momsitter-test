28364b0db23920ec3def21f583d230f09f063ff8
thedev-junyoung <thedev.junyoung@gmail.com>
2025-06-25 21:44:16 +0900

    chron: jwt 환경 설정

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,6 +28,11 @@ dependencies {
 
 	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.7.0")
 
+	implementation("io.jsonwebtoken:jjwt-api:0.11.5")
+	runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
+	runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
+
+
 
 	// Test
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
