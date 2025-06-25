d007c547c3f13a58dcc509718e98efc62c569084
thedev-junyoung <thedev.junyoung@gmail.com>
2025-06-26 01:16:05 +0900

    refactor: DUPLICATE_ROLE 추가

--- src/main/kotlin/com/momsitter/common/ErrorCode.kt
+++ src/main/kotlin/com/momsitter/common/ErrorCode.kt
@@ -5,4 +5,5 @@ import org.springframework.http.HttpStatus
 enum class ErrorCode(val status: HttpStatus, val message: String) {
     USER_NOT_FOUND(HttpStatus.BAD_REQUEST, "유저를 찾을 수 없습니다."),
     INVALID_PASSWORD(HttpStatus.BAD_REQUEST, "비밀번호가 일치하지 않습니다."),
+    DUPLICATE_ROLE(HttpStatus.BAD_REQUEST, "이미 존재하는 역할입니다."),
 }
