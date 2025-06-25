da50ee080e5c476d24a533e93d3080c33e301e05
thedev-junyoung <thedev.junyoung@gmail.com>
2025-06-25 20:37:10 +0900

    feat: 정적 팩토리 메소드 추가

--- src/main/kotlin/com/momsitter/application/user/dto/SignUpCommand.kt
+++ src/main/kotlin/com/momsitter/application/user/dto/SignUpCommand.kt
@@ -1,8 +1,10 @@
 package com.momsitter.application.user.dto
 
+import com.momsitter.domain.child.ChildInfo
 import com.momsitter.domain.parent.ParentProfileInfo
 import com.momsitter.domain.sitter.SitterProfileInfo
 import com.momsitter.domain.user.Gender
+import com.momsitter.presentation.user.dto.SignupRequest
 import java.time.LocalDate
 
 data class SignUpCommand(
@@ -16,4 +18,28 @@ data class SignUpCommand(
 
     val sitterInfo: SitterProfileInfo?,     // 시터로 가입할 경우만 입력
     val parentInfo: ParentProfileInfo?      // 부모로 가입할 경우만 입력
-)
+) {
+    companion object {
+        fun of(request: SignupRequest): SignUpCommand {
+            return SignUpCommand(
+                username = request.username,
+                rawPassword = request.password,
+                name = request.name,
+                birthDate = request.birthDate,
+                gender = Gender.valueOf(request.gender.uppercase()),
+                email = request.email,
+                roles = request.roles,
+                sitterInfo = request.sitter?.let {
+                    SitterProfileInfo.of(it.minAge, it.maxAge, it.introduction)
+                },
+                parentInfo = request.parent?.let {
+                    ParentProfileInfo.of(
+                        it.children.map { child ->
+                            ChildInfo.of(child.name, child.birthDate, Gender.valueOf(child.gender.uppercase()))
+                        }
+                    )
+                }
+            )
+        }
+    }
+}
