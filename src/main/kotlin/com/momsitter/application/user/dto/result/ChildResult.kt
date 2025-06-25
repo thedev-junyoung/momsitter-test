bf1845f08fe3b0e7be7aef4c47dfde005240fc55
thedev-junyoung <thedev.junyoung@gmail.com>
2025-06-26 01:30:45 +0900

    feat: 타입 변경

--- src/main/kotlin/com/momsitter/application/user/dto/result/ChildResult.kt
+++ src/main/kotlin/com/momsitter/application/user/dto/result/ChildResult.kt
@@ -1,18 +1,19 @@
 package com.momsitter.application.user.dto.result
 
 import com.momsitter.domain.child.Child
+import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 
 data class ChildResult(
     val name: String,
-    val birthDate: String,
+    val birthDate: LocalDate,
     val gender: String
 ) {
     companion object {
         fun from(child: Child): ChildResult {
             return ChildResult(
                 name = child.name,
-                birthDate = child.birthDate.format(DateTimeFormatter.BASIC_ISO_DATE),
+                birthDate = child.birthDate,
                 gender = child.gender.korName
             )
         }
