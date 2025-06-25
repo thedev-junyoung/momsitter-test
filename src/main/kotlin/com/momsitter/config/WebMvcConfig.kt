f4d96e84aa9e972998cc07c4d96969b75306d4c8
thedev-junyoung <thedev.junyoung@gmail.com>
2025-06-26 02:55:40 +0900

    refactor: 테스트에도 token 포함하도록 수정

--- src/main/kotlin/com/momsitter/config/WebMvcConfig.kt
+++ src/main/kotlin/com/momsitter/config/WebMvcConfig.kt
@@ -12,7 +12,7 @@ class WebMvcConfig {
     fun jwtFilter(jwtAuthenticationFilter: JwtAuthenticationFilter): FilterRegistrationBean<JwtAuthenticationFilter> {
         return FilterRegistrationBean<JwtAuthenticationFilter>().apply {
             filter = jwtAuthenticationFilter
-            addUrlPatterns("/api/v1/users/me", "/api/v1/care-requests/*") // 인증 필요한 경로만
+            addUrlPatterns("/*") // 인증 필요한 경로만
             order = 1
         }
     }
