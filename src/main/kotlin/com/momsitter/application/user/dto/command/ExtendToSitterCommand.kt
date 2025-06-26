cc97c3377474155cd29b40d7b2062b0ffb221987
thedev-junyoung <thedev.junyoung@gmail.com>
2025-06-26 13:02:25 +0900

    refactor: 팩토리 메서드 사용으로 변경

--- src/main/kotlin/com/momsitter/application/user/dto/command/ExtendToSitterCommand.kt
+++ src/main/kotlin/com/momsitter/application/user/dto/command/ExtendToSitterCommand.kt
@@ -5,4 +5,15 @@ data class ExtendToSitterCommand(
     val minCareAge: Int,
     val maxCareAge: Int,
     val introduction: String
-)
+) {
+    companion object {
+        fun of(userId: Long, minCareAge: Int, maxCareAge: Int, introduction: String): ExtendToSitterCommand {
+            return ExtendToSitterCommand(
+                userId = userId,
+                minCareAge = minCareAge,
+                maxCareAge = maxCareAge,
+                introduction = introduction
+            )
+        }
+    }
+}
